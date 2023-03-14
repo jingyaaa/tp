4d3fbef7740c010f636e87cf70a050e65abc2544
DESKTOP-ULU427O\jingy <jingya.lin02@gmail.com>
2023-03-14 22:55:15 +0800

    Add JUnit test for ingredients storage

---
+++ src/test/java/seedu/duke/storage/IngredientStorageTest.java
@@ -0,0 +1,53 @@
+package seedu.duke.storage;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
+import seedu.duke.DukeSession;
+import seedu.duke.ingredient.Ingredient;
+import seedu.duke.ingredient.IngredientList;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class IngredientStorageTest {
+
+    private final Ingredient ingredientToAdd1 = new Ingredient("apple", 2);
+    private final Ingredient ingredientToAdd2 = new Ingredient("carrot", 2.5);
+
+    @Test
+    public void writeIngredientToFile() throws IOException {
+        DukeSession dukeSession = new DukeSession();
+        dukeSession.getIngredientStorage().filename = "Output.txt";
+        dukeSession.getIngredientStorage().getFile(dukeSession.getIngredients());
+        dukeSession.getIngredientStorage().writeIngredientToFile(ingredientToAdd1);
+        dukeSession.getIngredientStorage().writeIngredientToFile(ingredientToAdd2);
+        String fileOutput = new String(Files.readAllBytes(Path.of("Output.txt")));
+        String fileExpected = new String(Files.readAllBytes(Path.of("Expected.txt")));
+        assertEquals(fileExpected, fileOutput);
+    }
+
+    @Test
+    public void writeIngredientsToFile() throws IOException {
+        DukeSession dukeSession = new DukeSession();
+        dukeSession.getIngredientStorage().filename = "Output.txt";
+        IngredientList ingredientList = new IngredientList();
+        ingredientList.add(ingredientToAdd1);
+        ingredientList.add(ingredientToAdd2);
+        dukeSession.getIngredientStorage().writeIngredientsToFile(ingredientList);
+        String fileOutput = new String(Files.readAllBytes(Path.of("Output.txt")));
+        String fileExpected = new String(Files.readAllBytes(Path.of("Expected.txt")));
+        assertEquals(fileExpected, fileOutput);
+    }
+
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        PrintWriter pw = new PrintWriter("Output.txt");
+        pw.close();
+    }
+
+}
